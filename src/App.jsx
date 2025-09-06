8c9fca08a295555fe551a5c894956318a3dde4a7
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-09-06 20:41:28 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { logout } from "./Redux/authSlice.js";
 import { setToken, setToastHandler } from "./Components/Action/Api";
 import { useToastr } from "./Components/Toastr/ToastrProvider";
 import { OPPS_MSG, SESSION_EXPIRE, EXPIRATION_TIME } from "./Utils/strings.js";
-import ValidateOtp from "./Pages/validateOtp/ValidateOtp.jsx";
+import ValidateOtp from "./Pages/ValidateOtp/ValidateOtp.jsx";
 
 import { persistor } from "./Redux/store";
 import PageNotFound from "./Pages/PageNotFound/PageNotFound.jsx";
